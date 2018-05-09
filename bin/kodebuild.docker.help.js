29a778bddc2449aef30a2cab7c15833f50e18182
Ivan Kluzak <ivan.kluzak@constellationmutual.com>
2018-05-09 10:30:56 -0500

    added snyk scan and tweaked help wording

--- bin/kodebuild.docker.help.js
+++ bin/kodebuild.docker.help.js
@@ -7,12 +7,12 @@
 function build_instructions() {
 
 	console.log("\n\n\n");
-	console.log("To build:\n");
+	console.log("To build the Docker image:\n");
 	console.log("\tgit clone https://github.com/ikluzak/aws-codebuild-docker-images.git");
 	console.log("\tcd aws-codebuild-docker-images/ubuntu/nodejs/6.3.1");
 	console.log("\tdocker build -t ikluzak/kodebuild-nodejs:6.3.1 . ");
 	console.log("\n\n");
-
+	console.log("Then just run kodebuild in your project directory with buildspec.yml\n");
 }
 
 module.exports = build_instructions;
